0583f68e8dfa565c8b3fc851444dd901dca60cdb
shripal <shripalp@gmail.com>
2023-05-08 22:14:53 -0600

    server and package.json updated

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.json());
 
 app.use("/api/portfolio", portfolioRoute);
 const port = process.env.PORT || 5000;
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
